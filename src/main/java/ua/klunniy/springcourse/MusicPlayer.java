d70275ebfdb1ae57364790cc5e1f1ba5173feadd
Serhii Klunniy <s.klunniy@gmail.com>
2023-08-20 23:30:39 +0300

    commit

--- src/main/java/ua/klunniy/springcourse/MusicPlayer.java
+++ src/main/java/ua/klunniy/springcourse/MusicPlayer.java
@@ -12,12 +12,17 @@ import ua.klunniy.springcourse.genreOfMusic.Music;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
         System.out.println(music.getSong());
+        System.out.println("name = " + name);
+        System.out.println("volume= " + volume);
     }
 
 }
