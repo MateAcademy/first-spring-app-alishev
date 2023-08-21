2bc8004e355964b4a36c757d4bcee6e4f64a3966
Serhii Klunniy <s.klunniy@gmail.com>
2023-08-21 13:21:48 +0300

    first commit add annotation

--- src/main/java/ua/klunniy/springcourse/genreOfMusic/impl/ClassicalMusic.java
+++ src/main/java/ua/klunniy/springcourse/genreOfMusic/impl/ClassicalMusic.java
@@ -1,10 +1,12 @@
 package ua.klunniy.springcourse.genreOfMusic.impl;
 
+import org.springframework.stereotype.Component;
 import ua.klunniy.springcourse.genreOfMusic.Music;
 
 /**
  * @author Serhii Klunniy
  */
+@Component("musicBean1")
 public class ClassicalMusic implements Music {
 
     private static Music instance;
